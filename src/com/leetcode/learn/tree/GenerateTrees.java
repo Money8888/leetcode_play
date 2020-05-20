09e8daa89b7aa6e7c5d3cb877f733f8cace9f998
Money8888 <13516139951@163.com>
2020-05-20 13:47:39 +0800

    leetcode刷题

---
+++ src/com/leetcode/learn/tree/GenerateTrees.java
@@ -0,0 +1,18 @@
+package com.leetcode.learn.tree;
+
+import java.util.List;
+
+/**
+ * 输入参数n
+ * 求节点为1到n所有可能的二叉搜索树
+ */
+public class GenerateTrees {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static List<TreeNode> generateTrees(){
+        
+    }
+}
